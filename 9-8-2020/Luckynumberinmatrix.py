d49acb532092c1a5baef27be39ce9b40ec986bfc
hoahanie <hoale160799@gmail.com>
2020-08-08 23:58:45 +0700

    Tìm lucky number

--- 9-8-2020/Luckynumberinmatrix.py
+++ 9-8-2020/Luckynumberinmatrix.py
@@ -1,12 +1,15 @@
+# https://leetcode.com/problems/lucky-numbers-in-a-matrix/
 class Solution:
     def luckyNumbers (self, matrix):
         newMatrix=[]
         lst=[]
+        # tìm ma trận chuyển vị
         for k in range(0,len(matrix[1])):
             subMatrix=[]
             for i in range(0,len(matrix)):
                 subMatrix.append(matrix[i][k])
             newMatrix.append(subMatrix)
+        # tìm lucky number
         for k in matrix:
             for i in range(0,len(k)):
                 for j in range(0,len(matrix)):
