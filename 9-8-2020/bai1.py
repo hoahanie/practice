7f5ebb62c637725bba240503cf2522fb9e236481
hoahanie <hoale160799@gmail.com>
2020-08-08 10:51:12 +0700

    giai bai1

--- 9-8-2020/bai1.py
+++ 9-8-2020/bai1.py
@@ -1,2 +1,19 @@
 # https://leetcode.com/problems/di-string-match/
 
+class Solution:
+    def diStringMatch(self, S):
+        n =len(S)
+        a = list(range(0,n+1))
+        res =[]
+        for i in S:
+            if i=='I':
+                res.append(a.pop(0))
+            else:
+                res.append(a.pop())
+        res.append(a.pop())
+                
+        return res
+        
+s=Solution()
+print(s.diStringMatch("IDID"))
+        
