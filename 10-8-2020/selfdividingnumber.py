9fdb2e8ea13df397cd953e88b3cb72475d252ace
hoahanie <hoale160799@gmail.com>
2020-08-11 23:25:34 +0700

    selfdividingnumber finished

--- 10-8-2020/selfdividingnumber.py
+++ 10-8-2020/selfdividingnumber.py
@@ -4,19 +4,22 @@ class Solution:
         a=list(str(n))
         lst=[]
         for i in a:
-            if i!='0':
-                lst.append(int(i))  
+            lst.append(int(i))  
         return lst
     def selfDividingNumbers(self, left, right):
         res=[]
         for i in range(left,right+1):
-            for k in self.numberFactors(i):
-                if i% k!=0:
-                    break
-            else:
-                res.append(i)
+            if 0 not in self.numberFactors(i):
+                check =True
+                for k in self.numberFactors(i):
+                    if i% k!=0:
+                        check =False
+                        break
+                if check ==True:
+                    res.append(i)
         return res
 s=Solution()
-print(s.numberFactors(20))
+print(s.selfDividingNumbers(1,25))
+
 
 
