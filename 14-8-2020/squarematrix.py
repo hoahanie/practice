2475b16a394cc8b75bd74331a14a4770df52a65e
hoahanie <hoale160799@gmail.com>
2020-08-14 23:52:01 +0700

    version 2 -squarematrix

--- 14-8-2020/squarematrix.py
+++ 14-8-2020/squarematrix.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/count-square-submatrices-with-all-ones/
 class Solution:
     def countSquares(self, matrix):
-        if len(matrix)==0 or len(matrix)==1:
+        if len(matrix)==0:
             return 0
 
         else:
