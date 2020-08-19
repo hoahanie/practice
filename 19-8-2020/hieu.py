91f937dcfb6c0a3f810730ed71abf96d3803c4d7
hoahanie <hoale160799@gmail.com>
2020-08-19 16:13:57 +0700

    tro ly ao cua Hoa Hanie

---
+++ 19-8-2020/hieu.py
@@ -0,0 +1,10 @@
+you  = "hello"
+if you == "":
+    robot_brain = "I can't hear you,try again"
+if you == "hello":
+    robot_brain = "Hello Hoa"
+elif you == "today":
+    robot_brain = " thu 6"
+else:
+    robot_brain = " I am fine. Thank you and you?"
+print(robot_brain)
