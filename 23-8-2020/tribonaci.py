e7fe25eb64787bdb1219a903ce24d9f0e23e6610
hoahanie <hoale160799@gmail.com>
2020-08-23 15:10:35 +0700

    AC

--- 23-8-2020/tribonaci.py
+++ 23-8-2020/tribonaci.py
@@ -1,12 +1,16 @@
 class Solution:
     def tribonacci(self, n):
-        if n ==0:
-            return 0
-        if n == 1:
-            return 1
-        if n ==2:
-            return 1
-        else:
-            return self.tribonacci(n-3)+self.tribonacci(n-2)+self.tribonacci(n-1)
+        # if n ==0:
+        #     return 0
+        # if n == 1:
+        #     return 1
+        # if n ==2:
+        #     return 1
+        # else:
+        #     return self.tribonacci(n-3)+self.tribonacci(n-2)+self.tribonacci(n-1)
+        lst =[0,1,1]
+        for i in range(3,n+1):
+            lst.append(lst[len(lst)-1]+lst[len(lst)-2]+lst[len(lst)-3])
+        return lst[n]
 s=Solution()
-print(s.tribonacci(25))
+print(s.tribonacci(5))
