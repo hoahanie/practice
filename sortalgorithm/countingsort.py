49f89a8fc49f5fdfbd4f3f7e95da8b88d7934181
hoahanie <hoale160799@gmail.com>
2020-08-26 14:30:38 +0700

    chưa hieu vong for thu 3

---
+++ sortalgorithm/countingsort.py
@@ -0,0 +1,22 @@
+class solution:
+    def counting_sort(self,input):
+        output= [0 for i in range(256)]
+        count=[0 for i in range(256)]
+        ans = ["" for _ in input] 
+        for i in input:
+            count[ord(i)]+=1
+        for i in range(256):
+            count[i]+=count[i-1]
+        for i in range(len(input)):
+            output[count[ord(input[i])]-1]=input[i]
+            count[ord(input[i])]-=1
+        print(output)
+        for i in range(len(input)):
+            ans[i]=output[i]
+        res ="".join(ans)
+        
+        return res
+
+
+s=solution()
+print(s.counting_sort("geeksforgeeks"))
