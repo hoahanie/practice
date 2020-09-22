be21704a15758294bb01aa7b58e62dad588e1cf9
hoahanie <hoale160799@gmail.com>
2020-09-22 14:28:19 +0700

    TLE 73/84

---
+++ 22-9-2020/productlessthanK.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/subarray-product-less-than-k/
+class Solution:
+    def numSubarrayProductLessThanK(self, nums, k):
+        count =0
+        for i in range(len(nums)):
+            a = self.countProduct(nums[i:],k)
+            count+=a
+        return count
+
+    def countProduct(self, num,k):
+        s=1
+        j=0
+        count1=0
+        while j<len(num):
+            if s*num[j]<k:
+                s=s*num[j]
+                count1+=1
+                j+=1
+            else:
+                break
+        return count1
+
+s=Solution()
+print(s.numSubarrayProductLessThanK([5, 2, 6], 100))
+        
+
+
+         
